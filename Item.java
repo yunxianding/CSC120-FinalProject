6be0c397a47850574b0983718f9601dce4cde292
yunxiann <yding41@smith.edu>
2025-04-14 23:37:02 -0400

    Added possible attributes

--- Item.java
+++ Item.java
@@ -1,6 +1,16 @@
 public class Item{
 
     //Attributes
-    
+    protected String name;
+    protected String description;
+    protected Boolean canBeGrabbed;
+    protected Boolean canBeSitOn;
+    protected Boolean canBePutItemsOn;
+    protected Boolean canBeSleptOn;
+    protected String inside;
+
+    /*Default Constructor */
+
+    //Items: bed, screwdriver, broom, table, chair, teddyBear, irisPot
 
 }
