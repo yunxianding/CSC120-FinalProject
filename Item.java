d3ee601724307666c7d887b9ad1600e664c86ace
yunxiann <yding41@smith.edu>
2025-04-15 23:11:08 -0400

    Finished first version of Item.java
    Notes:
    
    1.I deleted several items to start small and make it easier. We can always add more later.
    
    2. I added the two souls as instances of items. We can try setting the two souls as items instead of writing a soul class.
    
    3.I think printManifest of all the items should be in room classes instead of Item class. We could store the available items in an arrayList and print out.
    
    4. I'm unsure of whether this will work. Should we start a main class and put this main method in?

--- Item.java
+++ Item.java
@@ -1,16 +1,45 @@
 public class Item{
 
     //Attributes
-    protected String name;
-    protected String description;
-    protected Boolean canBeGrabbed;
-    protected Boolean canBeSitOn;
-    protected Boolean canBePutItemsOn;
-    protected Boolean canBeSleptOn;
-    protected String inside;
+    private String name;
+    private String description;
+    private Boolean canBeGrabbed;
 
-    /*Default Constructor */
+    /* Default Constructor */
+    public Item(String name, String description, Boolean canBeGrabbed){
+        this.name = name;
+        this.description = description;
+        this.canBeGrabbed = canBeGrabbed;
+    }
 
-    //Items: bed, screwdriver, broom, table, chair, teddyBear, irisPot
+    /**
+     * Accessor for item's name
+     * @return name of the item
+     */
+    public String getName(){
+        return this.name;
+    }
 
+    /**
+     * Accessor for item's description
+     * @return description of the item
+     */
+    public String getDescription(){
+        return this.description;
+    }
+
+    /**
+     * Main method that creates all the items
+     */
+    public static void main (String[] args){
+        Item bed = new Item("bed", "This is a bed.", false);
+        Item teddyBear = new Item("teddyBear", "This looks like a toy. It feels soft.", true);
+        Item firstSoul = new Item("firstSoul", "This is the first piece of my soul.", true);
+        Item box = new Item("box", "This is a box. Seems like there is something inside.", false);
+        Item leg = new Item("leg", "This looks like my leg. I'd better put it on to walk.", true);
+        Item irisPot = new Item("irisPot", "This is a plantpot. Seems like there is something inside.", false);
+        Item eye = new Item("eye", "This looks like my eye. I'd better put it on for better vision.", true);
+        Item computer = new Item("computer", "This is a computer. I want to check what's on it.", false);
+        Item secondSoul = new Item("secondSoul","This is the second piece of my soul.", true);
+    }
 }
