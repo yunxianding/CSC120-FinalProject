0a073077b86bc264833b8a14a4f2cc740580feff
tnphammy <tnpham@smith.edu>
2025-04-17 11:22:19 -0400

    sout test

--- Existence.java
+++ Existence.java
@@ -1,5 +1,47 @@
+import java.util.ArrayList;
+
 public class Existence {
     
     // Attributes
+    protected ArrayList<String> bodyParts = new ArrayList<>(); // List of body parts
+
+    // Methods
+    public void use(String s) {
+
+    }
+
+    public void touch(String s) {
+
+    }
+
+    public void grab(String s) {
+
+    }   
+
+    public void think() {
+        System.out.println("I am thinking");
+    }
+
+    public void fight() {
+        
+    }
+
+    public void lookAround() {
+
+    }
+
+    public void inspect() {
+
+    }
+
+    public void crawlTo(String s) {
+        
+    }
+
+
+    public void walkTo(String direction){
+        
+    }
+
     
 }
