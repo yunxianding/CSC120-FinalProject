68567e713e3712d47056858b845d34998c163fc4
yunxiann <yding41@smith.edu>
2025-04-17 19:09:05 -0400

    Created Main.java
    This is the class where we implement the classes and methods we have written and run the game. I currently created the items and two rooms. Much more to be added later, but this is a good start!

---
+++ Main.java
@@ -0,0 +1,35 @@
+/**
+ * The Main class is where we implement all the classes and run the game
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        //Create all the items needed
+        Item bed = new Item("bed", "This is a bed.", false);
+        Item teddyBear = new Item("teddyBear", "This looks like a toy. It feels soft.", true);
+        Item firstSoul = new Item("firstSoul", "This is the first piece of my soul.", true);
+        Item box = new Item("box", "This is a box. Seems like there is something inside.", false);
+        Item leg = new Item("leg", "This looks like my leg. I'd better put it on to walk.", true);
+        Item irisPot = new Item("irisPot", "This is a plantpot. Seems like there is something inside.", false);
+        Item eye = new Item("eye", "This looks like my eye. I'd better put it on for better vision.", true);
+        Item secondSoul = new Item("secondSoul","This is the second piece of my soul.", true);
+
+
+        //Create the Bedroom
+        Room bedroom = new Room("Bedroom", "This is a bedroom.");
+        bedroom.addItem(bed);
+        bedroom.addItem(teddyBear);
+        bedroom.addItem(firstSoul);
+        bedroom.addItem(box);
+        bedroom.addItem(leg);
+        bedroom.addItem(eye);
+        bedroom.addItem(irisPot);
+
+        //Create the Lab
+        Room lab = new Room("Lab", "This is a lab.");
+        lab.addItem(secondSoul); 
+        //Remember to add the computer after Computer.java is finished
+        //I don't know how to add the laser yet but we will figure it out
+
+    }
+}
