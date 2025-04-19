6800bf2253356bca159b454721dd970ad17e3bf9
tnphammy <tnpham@smith.edu>
2025-04-19 02:48:48 -0400

    Wrote most methods for existence class; overrode fight() in Human and Robot classes differently

--- Existence.java
+++ Existence.java
@@ -1,34 +1,126 @@
 import java.util.ArrayList;
 
 public class Existence {
-    
+
     // Attributes
-    protected ArrayList<String> bodyParts = new ArrayList<>(); // List of body parts
+    public String name;
+    protected ArrayList<Item> inventory = new ArrayList<>(); 
+    protected int health; // Imaginary health bar
+    public Boolean isAlive;
+
+    // Constructor
+
+    /**
+     * Default player constructor
+     */
+    public Existence(String name, int health,  Boolean isAlive) {
+        this.name = "Teddy";
+        this.isAlive = true;
+        this.health = 100; // User starts the game with full health.
+        this.inventory = new ArrayList<Item>();
+    }
 
     // Methods
-    public void use(String s) {
 
+    // Accessors / changers
+
+    public String getName() {
+        return this.name;
+    }
+    /**
+     * Getter for one's health bar
+     * @return Current health bar number
+     */
+    public int getHealth() {
+        return this.health;
     }
 
-    public void touch(String s) {
+    /**
+     * Decreases health (while fighting)
+     */
+    public void weaken() {
+        this.health -= 20;
+    } 
 
+    /**
+     * Immediately changes one's isAlive state to false, and health to 0.
+     * Often as a result of being electrocuted or hit by a laser beam.
+     */
+    public void die() {
+        this.health = 0;
+        this.isAlive = false;
     }
 
-    public void grab(String s) {
+    // Game Functions
 
+    /**
+     * Browse all, and utilise a functionality of an item
+     */
+    public void use(Item s) {
+        if(inventory.contains(s)) {
+            System.out.println(s.getDescription()); // Placeholder for .getUse()
+        }
+        else {
+            throw new RuntimeException("You to not have " + s.getName() + ". Take it first!.");
+        }
+    }
+
+    /**
+     * Touch an item, which gives its description, NOT add it to inventory.
+     * @param s The item
+     */
+    public void touch(Item s) {
+        System.out.println(s.getDescription());
+    }
+
+    /**
+     * Lets the user take/own an item (add it to inventory)
+     * @param s the item
+     */
+    public void take(Item s) {
+        if(this.inventory.contains(s)) {
+            throw new RuntimeException("You already have " + s.getName() + ".");
+        }
+        else {
+            this.inventory.add(s);
+            System.out.println(s.getName() + " successfully added to your inventory!");
+        }
     }   
 
-    public void think() {
-        System.out.println("I am thinking");
+    /**
+     * Print the ArrayList of player's inventory.
+     */
+    public void printInventory() {
+        System.out.println("Your inventory contains:");
+        System.out.println(this.inventory.toString());
     }
 
-    public void fight() {
-        
+    /**
+     * Lets the user access history of an item
+     * @param s The item
+     */
+    public void think(Item s) {
+        System.out.println("I am thinking...");
+        // s.getHistory() //Placeholder for that as well
     }
 
-    public void lookAround() {
-
+    /**
+     * User fights another existence for a specified number of times
+     * @param e The other entity
+     * @param times The number of times to fight
+     */
+    public void fight(Existence e, int times) {
+        System.out.println("I am fighting!");
     }
+
+    /**
+     * See description of the surrounding space.
+     * @param r The room one is in
+     */
+    public void lookAround(Room r) {
+        System.out.println("You are in: " + r.getDescription());
+    }
+
 
     public void inspect() {
 
@@ -42,6 +134,7 @@ public class Existence {
     public void walkTo(String direction){
         
     }
+
 
     
 }
--- Human.java
+++ Human.java
@@ -1,3 +1,37 @@
 public class Human extends Existence {
+    // Attributes
 
+
+    // Constructor
+
+    /**
+     * Default constructor for 'Human'
+     * @param name
+     * @param health
+     * @param isAlive
+     */
+    public Human(String name, int health,  Boolean isAlive) {
+        super(name, health, isAlive);
+    }
+
+
+    // Methods
+    
+
+    // To overload
+    /**
+     * User fights another existence
+     */
+    public void fight(Robot e, int times) {
+        for(int i = 0; i <= times; i++) {
+            this.health -= 20;
+            System.out.println("I am fighting!");
+        }
+            System.out.println("I am so tired, but I can keep going!");
+
+
+    }
+
+
+    // To override
 }
