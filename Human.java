7c3eb0f9a59656c443c8c2f5ee78c0b0aa8001e7
yunxiann <yding41@smith.edu>
2025-04-27 14:43:38 -0400

    Added attribute isUnconscious, deleted die and fight methods
    The die method is not necessary because we won't let the player command themselves to die after they exchanged back to the human body.
    The fight method is also unnecessary because the player won't fight the robot anymore when  they have exchanged back to the human body.

--- Human.java
+++ Human.java
@@ -1,17 +1,32 @@
+import java.util.ArrayList;
+
+/**
+ * The Human class extends Existence class and has unique features of ???TO BE ADDED
+ */
 public class Human extends Existence {
     // Attributes
-
+    public Boolean isUnconscious;
 
     // Constructor
 
     /**
      * Default constructor for 'Human'
      * @param name
-     * @param health
+     * @param room
      * @param isAlive
+     * @param canGoToLab
+     * @param canWalk
+     * @param canInspect
      */
-    public Human(String name, int health,  Boolean isAlive) {
-        super(name, health, isAlive);
+    public Human(String name, Room room, Boolean isAlive, Boolean canGoToLab, Boolean canWalk, Boolean canInspect) {
+        super(name, room);
+        this.isAlive = true;
+        this.health = 1000; // Human has more health so that a robot can't fight over a human.
+        this.inventory = new ArrayList<Item>();
+        this.canGoToLab = true;
+        this.canWalk = true;
+        this.canInspect = true;
+        this.isUnconscious = false;
     }
 
 
@@ -19,31 +34,6 @@ public class Human extends Existence {
     
 
     // To overload
-    /**
-     * Immediately changes one's isAlive state to false, and health to 0.
-     * Often as a result of being electrocuted or hit by a laser beam.
-     */
-    public void die() {
-        this.health = 0;
-        this.isAlive = false; 
-        respawnChat();
-    }
-
-    /**
-     * User fights another existence
-     * @param e The robot to be fought
-     * @param times The number of times to fight
-     */
-    public void fight(Robot e, int times) {
-        for(int i = 0; i <= times; i++) {
-            this.health -= 20;
-            System.out.println("I am fighting!");
-        }
-            System.out.println("I am so tired, but I can keep going!");
-
-
-    }
-
-
+    
     // To override
 }
