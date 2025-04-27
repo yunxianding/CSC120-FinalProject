1de5272dc95b2a26c850da9fbf0fa99bd1e169d0
yunxiann <yding41@smith.edu>
2025-04-27 14:45:06 -0400

    Added boolean attributes and `putOn` method
    Also updated electrocute method to set the human status to unconscious

--- Robot.java
+++ Robot.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * The Robot class extends Existence class and has the unique method of electrocute human
+ */
 public class Robot extends Existence {
+
+    //Attributes
+    Boolean poweredOn;
+    Boolean reasoningOn;
+    Boolean memoryOn;
 
     /**
      * Default constructor for 'Human'
@@ -6,12 +16,36 @@ public class Robot extends Existence {
      * @param health
      * @param isAlive
      */
-    public Robot(String name, int health,  Boolean isAlive) {
-        super(name, health, isAlive);
+    public Robot(String name, Room room, Boolean isAlive, Boolean canGoToLab, Boolean canWalk, Boolean canInspect) {
+        super(name, room);
+        this.isAlive = true;
+        this.health = 100; // Robot has less health than human.
+        this.inventory = new ArrayList<Item>();
+        this.canGoToLab = false; // Will unlock after achieving the first four tasks in stage 1
+        this.canWalk = false; // Will unlock after adding leg to the body
+        this.canInspect = true; // Will unlock after adding eye to the body
+        this.poweredOn = true; // Will be able to turn off after unlocking the control panel
+        this.reasoningOn = false; // Will be able to turn on after unlocking the control panel
+        this.memoryOn = false; // Will be able to turn on after unlocking the control panel
     }
     
     // Methods
+    /**
+     * The robot can put on items that are mechanical parts of its body
+     * @param item s
+     */
+    public void putOn(Item s) {
+        if (this.inventory.contains(s) && s.getCanBePutOn() == true) {
+            System.out.println("You have put on " + s.getName() + ".");
+            this.inventory.remove(s);
+        } // To be contructed
+    }
+
+    /**
+     * The robot can electrocute the human and make the human fall unconscious
+     */
     public void electrocute(Human h) {
+        h.isUnconscious = true;
         System.out.println(h.getName() + " is temporarily unconscious.");
     }
 
@@ -19,18 +53,6 @@ public class Robot extends Existence {
 
     // To override
 
-    /**
-     * User fights another existence
-     */
-    public void fight(Human h, int times) {
-        for(int i = 0; i <= 3; i++) {
-            this.health -= 20;
-            System.out.println("I am fighting!");
-        }
-            System.out.println("I am so tired, maybe I need to try something else...");
-
-    }
-    
     
 
 }
