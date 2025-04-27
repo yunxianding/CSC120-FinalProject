2f28a8f7f6292e54ca1d97f2086630d478aa9d6b
yunxiann <yding41@smith.edu>
2025-04-27 14:38:59 -0400

    Updated `walkTo`, `crawlTo` , overloaded `open`
    There are still many things to be constructed in the Existence class. See the comments.

--- Existence.java
+++ Existence.java
@@ -1,42 +1,51 @@
-
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-
+/**
+ * The Existence class represents the existences that can interact with the items
+ */
 public class Existence {
 
     // Attributes
     public String name;
+    public Room currentRoom; // To be discussed! Do we need this?
+    // Should we also have an attribute for currentLocation? Like near some item?
     protected ArrayList<Item> inventory = new ArrayList<>(); 
     protected int health; // Imaginary health bar
     public Boolean isAlive;
-    private Object currentRoom;
-    private Object currentTarget;
-    private Boolean canGoToLab;
+    protected Boolean canGoToLab;
+    protected Boolean canWalk;
+    protected Boolean canInspect;
 
     // Constructor
 
     /**
      * Default player constructor
+     * @param name
+     * @param currentRoom
      */
-    public Existence(String name, int health,  Boolean isAlive) {
+    public Existence(String name, Room currentRoom) {
         this.name = "Teddy";
         this.isAlive = true;
         this.health = 100; // User starts the game with full health.
         this.inventory = new ArrayList<Item>();
         this.canGoToLab = false; // Default to false
+        this.canWalk = false; // Default to false because the player wake up with one lege missing
+        this.canInspect = false; // Default to false because the player wake up with one eye missing
 
     }
 
     // Methods
 
-    // Accessors / changers
-
+    /**
+     * Accessor for an exsitence's name
+     * @return name of the existence
+     */
     public String getName() {
         return this.name;
     }
+
     /**
-     * Getter for one's health bar
+     * Accessor for one's health bar
      * @return Current health bar number
      */
     public int getHealth() {
@@ -44,7 +53,7 @@ public class Existence {
     }
 
     /**
-     * Get user's isAlive value (true/false)
+     * Accessor for user's isAlive value (true/false)
      */
     public Boolean getIsAlive() {
         return this.isAlive;
@@ -58,6 +67,20 @@ public class Existence {
     }
 
     /**
+     * Accessor for the player's state of whether they can walk yet (only with both legs can)
+     */
+    public Boolean getCanWalk() {
+        return this.canWalk;
+    }
+
+    /**
+     * Accessor for the player's state of whether they can inspect yet (only with both eyes can)
+     */
+    public Boolean getCanInspect() {
+        return this.canInspect;
+    }
+    
+    /**
      * Decreases health (while fighting)
      */
     public void weaken() {
@@ -69,67 +92,108 @@ public class Existence {
      * Often as a result of being electrocuted or hit by a laser beam.
      */
     public void die() {
-        this.health = 0;
-        this.isAlive = false; 
+        if (this.health <= 0) {
+            this.isAlive = false;
+            System.out.println("You have died.");
+            respawnChat();
+        }
     }
 
     /**
      * Become alive again
      */
     public void respawn() {
-        this.getIsAlive();
         if(this.isAlive == false) { // Check that user is actually dead
             this.health = 100;
             this.isAlive = true;
             System.out.println("I am reborn!");
-        }
-        else {
+        } else {
             throw new RuntimeException("You are still alive! ");
         }
     }
 
-/**
-   * Starts and runs the conversation with the user
-   * Asks the user how many rounds they want to chat for
-   */
-  public void respawnChat() {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Do you want to die or respawn to play again? Press Y to respawn and N to die.");
+    /**
+    * Starts and runs the conversation with the user
+    * Asks the user how many rounds they want to chat for
+    */
+    public void respawnChat() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to die or respawn to play again? Press Y to respawn and N to die.");
 
-    String choice = scanner.nextLine();
-    if (choice.equals("Y")) {
-        this.getIsAlive();
-        this.respawn();
-        System.out.println("You're alive again - Hooray!");
-    }
-    else if (choice.equals("N")) {
-        System.out.println("Okay, you're dead. Bye!");
-    }
-    else {
-        System.out.println("I don't understand what you wrote. Try only Y or N.");
-    }
-    scanner.close();
+        String choice = scanner.nextLine();
+        if (choice.equals("Y")) {
+            this.respawn();
+            System.out.println("You're alive again - Hooray!");
+        } else if (choice.equals("N")) {
+            System.out.println("Okay, you're dead. Bye!");
+        } else {
+            System.out.println("I don't understand what you wrote. Try only Y or N.");
+        }
+        scanner.close();
     }
 
     // Game Functions
 
-    public void open(Item s){
+    /**
+     * Open an item if there is another item stored inside
+     * @param item to be opened
+     */
+    public void open(Item s) {
         if (s.getCanBeOpened() == true){
             s.getContainedItem();
-            System.out.println(s + " contains " + s.getContainedItem());
+            System.out.println("You have opened " + s + ". Inside it is " + s.getContainedItem() + " .");
+        } else {
+            // To be constructed! A few scenorios:
+            // 1. try to open an item that can't be opened
+            // 2. try to open an item that has already been opened
+            // 3. try to open an item when we are not near it's location
+            // Some more?
         }
     }
+
     /**
-     * Browse all, and utilise a functionality of an item
+     * Overloaded oepn method for a computer
+     * @param computer to be opened
      */
-    public void use(Item s) {
-        if(inventory.contains(s)) {
-            System.out.println(s.getDescription()); // Placeholder for .getUse()
-        }
-        else {
-            throw new RuntimeException("You to not have " + s.getName() + ". Take it first!.");
-        }
+    public void open(Computer c) {
+        if (c.locked == true) {
+            c.locked = false;
+            System.out.println("\nYou opend the computer. Two folders appear: History and Control Panel.");
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Which folder do you want to see? Press H to open History folder and C to open Control Panel.");
+            String choice2 = scanner2.nextLine();
+            if (choice2.equals("H")) {
+                System.out.println("You opened the History Folder...");
+                c.openHistory();
+            } else if (choice2.equals("C")) {
+                System.out.println("You opened the control Panel...");
+                c.openControlPanel(Robot r, Room rm);
+                System.out.println("Do you want to toggle any controls? Enter the name of the control to toggle... ");
+                System.out.println("Do you want to toggle any controls? Enter the name of the control to toggle... ");
+                /** Scanner scanner3 = new Scanner(System.in);
+                String choice3 = scanner3.nextLine();
+                if (choice3.equals("Reasoning")) {
+                    c.toggleReasoning(Robot r);
+                } else if (choice3.equals("Power")) {
+                    c.togglePower(Robot r);
+                } else if (choice3.equals("Memory")) {
+                    c.toggleMemory(Robot r);
+                } else if (choice3.equals("Laser")) {
+                    c.toggleLaser(Room r);
+                } else if (choice3.equals("TradeBody") || choice3.equals("SelfDestruct")) {
+                    System.out.println("\n[RESTRICTED ACTION FAILED]");
+                } else {
+                    System.out.println("[INVALID COMMAND]");
+                } scanner3.close(); */
+            } else {
+                System.out.println("[INVALID COMMAND]");
+            } scanner2.close();
+        } else {
+            System.out.println("\nThe computer is unlocked and opened.");
+        } //Much to do with this overloaded method!!
     }
+
+    
 
     /**
      * Touch an item, which gives its description, NOT add it to inventory.
@@ -140,18 +204,32 @@ public class Existence {
     }
 
     /**
-     * Lets the user take/own an item (add it to inventory)
+     * Let the user take/own an item (add it to inventory)
      * @param s the item
      */
     public void take(Item s) {
         if(this.inventory.contains(s)) {
             throw new RuntimeException("You already have " + s.getName() + ".");
-        }
-        else {
+        } else {
             this.inventory.add(s);
             System.out.println(s.getName() + " successfully added to your inventory!");
         }
     }   
+
+    /**
+     * Let the user put down an item at a room (remove it from inventory)
+     * @param s the item
+     * @param r the room
+     */
+    public void putDown(Item s, Room r) {
+        if (this.inventory.contains(s) && r != null) {
+            this.inventory.remove(s);
+            System.out.println("You put down the " + s.getName() + " in the " + r.getName() + ".");
+        } // This method need to be modified.
+          // We need to figure out how to how to represent the current room we are in
+          // If we want to put down the item in the current room, we will successfully do that
+          // If we want to put down the item in another room, we need to walk to/crawl to that room first
+    }
 
     /**
      * Print the ArrayList of player's inventory.
@@ -162,20 +240,11 @@ public class Existence {
     }
 
     /**
-     * Lets the user access history of an item
-     * @param s The item
-     */
-    public void think(Item s) {
-        System.out.println("I am thinking...");
-        // s.getHistory() //Placeholder for that as well
-    }
-
-    /**
-     * User fights another existence for a specified number of times
+     * User fights another existence
      * @param e The other entity
-     * @param times The number of times to fight
      */
-    public void fight(Existence e, int times) {
+    public void fight(Existence e) {
+        this.weaken();
         System.out.println("I am fighting!");
     }
 
@@ -185,23 +254,15 @@ public class Existence {
      */
     public void lookAround(Room r) {
         if (r == null) {
-        System.out.println("You’re not in a room.");
+        System.out.println("You're not in a room.");
         return;
+        } System.out.println("You look around the: " + r.getName() + r.getDescription());
     }
 
-    System.out.println("You look around the room: " + r.getName());
-    List<Item> items = r.getItems();
-    if (items.isEmpty()) {
-        System.out.println("The room seems empty.");
-    } else {
-        System.out.println("You see the following items:");
-        for (Item item : items) {
-            System.out.println("- " + item.getName());
-        }
-    }
-    }
-
-    
+    /**
+     * Inspect an item, see its description and the status of whether it can be grabbed
+     * @param s the item to be inspected
+     */
     public void inspect(Item s) {
         if (s == null) {
             System.out.println("There is nothing to inspect.");
@@ -209,53 +270,45 @@ public class Existence {
         }
     
         System.out.println("Inspecting the " + s.getName() + "...");
-        System.out.println("Description: " + s.getDescription());
-        System.out.println("Can be grabbed? " + (s.getItemStatus() ? "Yes" : "No"));
+        System.out.println("Description: " + s.getDescription() + s.getItemStatus());
     }
     
-    public void walkTo(Item s) {
-        if (s == null) {
-            System.out.println("There is nowhere to walk to.");
-            return;
+    /**
+     * Walk to an item in a room (only with both legs)
+     * @param r the room to walk to
+     * @param s the item to walk to
+     */
+    public void walkTo(Room r, Item s) {
+        if (this.canWalk = true) {
+            System.out.println("You can walk to these items in the current room" + r.getItems());
+            if (r == null || s == null) {
+                System.out.println("There is nowhere to walk to.");
+            } else if (!r.getItems().contains(s)) {
+                System.out.println("You can't walk to the " + s.getName() + " because it's not in this room.");
+            } else {
+                System.out.println(" You walked to " + s.getName() + ".");
+                System.out.println("... Walking ...");
+                System.out.println("You are now near the " + s.getName() + ".");
+            }
+        } else {
+            System.out.println("You don't have both legs yet. You can only crawl to somewhere.");
         }
-    
-    
-        if (currentTarget != null && currentTarget.equals(s)) {
-            System.out.println("You’re already near the " + s.getName() + ".");
-            return;
-        }
-    
-        if (currentRoom != null && !((Room) currentRoom).getItems().contains(s)) {
-            System.out.println("You can't walk to the " + s.getName() + " — it’s not in this room.");
-            return;
-        }
-    
-        currentTarget = s;
-        System.out.println(" You walk to " + s.getName() + ".");
-        System.out.println("You are now near the " + s.getName() + ".");
     }
     
-
-
-    public void crawlTo(Item s) {
-        if (s == null) {
-            System.out.println("There is nowhere to crawl to.");
-            return;
+    /**
+     * Crawl to an item in a room
+     * @param r the room to crawl to
+     * @param s the item to crawl to
+     */
+    public void crawlTo(Room r, Item s) {
+        if (r.getItems().contains(s)) {
+            System.out.println("You crawled slowly toward the " + s.getName() + ".");
+            System.out.println("....... Crawling ...... \n ...... Crawling ......");
+            System.out.println("..... Almost there ......");
+            System.out.println("You reached the " + s.getName() + ", catching your breath.");
+            System.out.println("You are now near the " + s.getName() + ".");
+        } else {
+            System.out.println("You can't crawl to the " + s.getName() + " because it's not in this room.");
         }
-    
-        if (currentTarget != null && currentTarget.equals(s)) {
-            System.out.println("You’re already near the " + s.getName() + ".");
-            return;
-        }
-    
-        if (currentRoom != null && !((Room) currentRoom).getItems().contains(s)) {
-            System.out.println("You can't crawl to the " + s.getName() + " — it’s not in this room.");
-            return;
-        }
-    
-        currentTarget = s;
-        System.out.println(this.name + " crawls slowly toward the " + s.getName() + ".");
-        System.out.println("You reach the " + s.getName() + ", catching your breath.");
     }
-    
 }
