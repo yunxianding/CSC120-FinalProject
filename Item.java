56826104785b03a0da8acbc2b9873dac069f97ec
yunxiann <yding41@smith.edu>
2025-04-27 14:36:08 -0400

    Added boolean `canBePutOn` and method `getItemStatus`
    I added boolean `canBePutOn` for items like leg and eye that can be put on.
    I also updated method `getItemStatus` so that its gets the three status at once.

--- Item.java
+++ Item.java
@@ -2,7 +2,7 @@
  * The Item class creates items we will use in the rooms.
  * Each item has a name, description and a status of whether can be grabbed.
  */
-public class Item{
+public class Item {
 
     //Attributes
     private String name;
@@ -10,21 +10,23 @@ public class Item{
     private Boolean canBeGrabbed;
     private Item containedItem;
     private Boolean canBeOpened;
+    private Boolean canBePutOn;
 
     /* Default Constructor */
-    public Item(String name, String description, Boolean canBeGrabbed){
+    public Item(String name, String description, Boolean canBeGrabbed, Boolean canBeOpened, Boolean canBePutOn) {
         this.name = name;
         this.description = description;
         this.canBeGrabbed = canBeGrabbed;
         this.containedItem = null;
         this.canBeOpened = false;
+        this.canBePutOn = false;
     }
 
     /**
      * Accessor for item's name
      * @return name of the item
      */
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
@@ -32,24 +34,43 @@ public class Item{
      * Accessor for item's description
      * @return description of the item
      */
-    public String getDescription(){
+    public String getDescription() {
         return this.description;
     }
 
     /**
-     * Accessor for item's status(whether it can be grabbed)
-     * @return the status(whether it can be grabbed) of the item
+     * Accessor for item's status(whether it can be grabbed/opened/put on)
+     * @return the statusof the item
      */
-    public Boolean getItemStatus(){
+    public String getItemStatus() {
+        return ("Item status for " + this.getName() +
+                "\n can be grabbed: " + this.canBeGrabbed +
+                "\n can be opened: " + this.canBeOpened +
+                "\n can be put on: " + this.canBePutOn);
+    }
+
+    /**
+     * Accessor for canBeGrabbed
+     * We wrote a separate accessor for this because it will be used in the grab method for existence
+     */
+    public Boolean getCanBeGrabbed() {
         return this.canBeGrabbed;
     }
-    
+
     /**
-     * Accessor for whether an item can be opened
-     * @return whether the item can be opened(whether it contains another item)
+     * Accessor for canBeOpened
+     * We wrote a separate accessor for this because it will be use in the open method for existence
      */
-    public Boolean getCanBeOpened(){
+    public Boolean getCanBeOpened() {
         return this.canBeOpened;
+    }
+
+    /**
+     * Accessor for canBePutOn
+     * We wrote a separate accessor for this because it will be used in the putOn method for robots
+     */
+    public Boolean getCanBePutOn() {
+        return this.canBePutOn;
     }
 
     /**
@@ -69,12 +90,5 @@ public class Item{
         } else {
             throw new RuntimeException("This item already contains another item.");
         }
-    }
-
-    /**
-     * Method for removing the stored item if the player interacts with the container item
-     */
-    public void removeItem(Item item){
-        //To be constructed: if player takes it, remove the stored item
     }
 }
