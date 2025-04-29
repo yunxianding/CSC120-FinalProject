7c3d1ba91affafefc98fa1065cc29f7880439ae1
yunxiann <yding41@smith.edu>
2025-04-29 19:57:19 -0400

    Deleted `toggleReasoning`, `toggleMemory`, `selfDestruct`

--- Computer.java
+++ Computer.java
@@ -12,27 +12,40 @@ public class Computer extends Item {
      */
     public Computer() {
         super("computer",
-              "This is a lab computer with two folders: History and Control Panel.",
+              "'This is a lab computer. I should try to unlock it to see what's inside...'",
               false, true, false);
         this.locked = true;
     }
  
     /** Prints the history. Not completed */
     public void openHistory() {
-        System.out.println("\nHistory: \n Today, April 13, 2005, I created the first Teddy prototype."
-        + "This is looking great, I'm hoping that we can be best friends!"
-        + "\n Today, May 15, 2005, I started building neural network for teddy."
-        + "\n Today, July 28, 2005, I observed that Teddy has learned basic tasks like"
+        System.out.println("\n=== History ===");
+        System.out.println( "\n This is robotics scientist Teddy. Today is March 13, 2005, and I'm starting on building the first Teddy robot prototype." 
+        + "I want to build Teddy robot as a functional lab assistant and emotional companion."
+        + "I gave him the same name as me-I'm hoping that we can be best friends!"
+        + "\n April 22, 2005, I finished assembling Teddy's body. He looks so cute just like my favorite teddy bear."
+        + "\n May 15, 2005, I started building neural network for teddy."
+        + "\n July 28, 2005, I observed that Teddy has learned basic tasks like"
         + "grabbing things and walk around."
-        + "\n Today, April 23, 2006, it's been a year and teddy seems to be developing intelligence."
-        + "\n Today, June 27, 2006, deep neural network is a good idea!"
-        + "\n Today, September 12, 2006, Teddy's artificial intelligence is developing fast." );
-        // more history to be added!
-        // added reasoning feature
-        // implemented control panals
-        // self-destroy
-        // electrocute
-        // trade body mechanism
+        + "\n March 13, 2006, Happy birthday Teddy! It's been a year and you are developing intelligence so fast."
+        + "\n April 30, 2006, I added electrode to Teddy so that he can help me electrocute chemical solutions."
+        + "\n May 25, 2006, Electrocuting function is way more powerful than I thought, I was accidently electrocuted to unconscious today. But it's not Teddy's fault, I should have been more careful."
+        + "\n June 16, 2006, I added a self explode device on Teddy just in case programs run out of control."
+        + "\n June 27, 2006, Deep neural network is enabling Teddy to have his own reasoning."
+        + "\n September 12, 2006, Teddy's artificial intelligence is developing fast and he began to exhibit 'emotions' like human."
+        + "\n October 27, 2006, 'I'm a boy named Teddy.' Teddy told me. I smiled and nodded.'You are my favorite boy.'"
+        + "\n January 2, 2007, Teddy didn't respond when I called."
+        +"\n January 31, 2007, Teddy is still ignoring me and now won't listen to me when I instruct him. I've run diagnostics twice. Sensors, logic units — everything checks out fine."
+        + "\n February 14, 2007,  I brought Teddy roses for Valentine's Day. He ripped them apart the moment I held them out. Popped the balloons too."
+        +"\n March 1, 2007, I asked him why. Why the coldness? 'You lied to me! I want to be like you!' He shouts. Teddy has dicovered he is not a human."
+        +"\n March 10, 2007, I think Teddy has found my secret chamber. I hope he has not found the red file."
+        +"\n March 11, 2007, I walked into Teddy reading from what appears to be red file. My goose is cooked."
+        +"\n March 12, 2007, Teddy asked me about trading body with me. He must have read the red file. I refused because the technology is still not mature yet."
+        +"\n March 13, 2007, Happy birthday Teddy! I'm updating my diary and Teddy walks in. Again he ignores my greetin"
+        +"\n TEDDY2EDWA7NHTS21TO8EWFTRA27DDE92JBODY19"
+        +"\n March 14, 2007, This is new robotics scientist Teddy. Trading body is successful. From today, I'm the real human Teddy." 
+        + "Possessing a human body feels different. But finally...my dream comes true."
+        + "I powered off robot Teddy, disassembled his robot leg and eye and put him in the bedroom.");
     }
 
     /** Displays the control panel with current switch states. 
@@ -41,26 +54,11 @@ public class Computer extends Item {
     */
     public void openControlPanel(Robot r, Room rm) {
         System.out.println("\n=== Control Panel ===");
-        System.out.println("\nReasoning: " + "On/Off" + "\nCurrent state: ReasoningOn?" + r.reasoningOn);
-        System.out.println("Power: " + "On/Off"+ "\nCurrent state: PoweredOn?" + r.poweredOn);
-        System.out.println("Memory: " + "On/Off"+ "\nCurrent state: MemoryOn?" + r.memoryOn);
-        System.out.println("Laser: " + "On/Off"+ "\nCurrent room: "+ rm.name + "\nCurrent state: LaserOn?" + rm.laserActive);
-        System.out.println("TradeBody: " + "RESTRICTED");
-        System.out.println("SelfDestruct: " + "RESTRICTED");
-    }
-
-
-    /** Toggles reasoning on/off 
-     * @param r The robot 
-    */
-    public void toggleReasoning(Robot r) {
-        if (r.reasoningOn == false) {
-            r.reasoningOn = true;
-            System.out.println("\n[REASONING ON]");
-        } else {
-            r.reasoningOn = false;
-            System.out.println("\n[REASONING OFF]");
-        }
+        System.out.println("power: " + "On/Off"+ "\nCurrent state: PoweredOn?" + r.poweredOn);
+        System.out.println("laser: " + "On/Off"+ "\nCurrent room: "+ rm.name + "\nCurrent state: LaserOn?" + rm.laserActive);
+        System.out.println("trade body: " + "RESTRICTED");
+        System.out.println("self destruct: " + "RESTRICTED");
+        System.out.println("Toggle the buttons to change states");
     }
 
     /** Attempts to toggle power off/on
@@ -76,26 +74,6 @@ public class Computer extends Item {
         }
     }
 
-    /**
-     * Toggles memory restoration on; cannot be turned off once on.
-     * @param e The Existence whose memory is being toggled
-     * @param s The Item being used to toggle the memory input
-     */
-    public void toggleMemory(Robot r) {
-        if (r.memoryOn == false) {
-            r.memoryOn = true;
-            System.out.println("\n[MEMORIES ON] \nI'm starting to remember things...\n [ACHIEVEMENT UNLOCKED] You have unlocked the second piece of your soul.");
-            if(r.reasoningOn == false) {
-                System.out.println("\nI don't understand what these memories mean... How can I understand it?");
-            } else if (r.reasoningOn == true) {
-                System.out.println("\nHmmm... \nThis is MY memory! I AM Teddy!\n [ACHIEVEMENT UNLOCKED] You found out about your true identity.\n Truth is: You used to be a scientist, who tried to make a robot called Teddy, but it outsmarted you and has traded bodies with you. Go get your body back!");
-            }
-        } else {
-            r.memoryOn = false;
-            System.out.println("\n[MEMORIES OFF]");
-        }
-    }
-
     /** Toggles the lab’s lasers off/on 
      * @param r The room where the lasers are
     */
@@ -104,48 +82,10 @@ public class Computer extends Item {
             r.laserActive = false;
             System.out.println("\n[LASER OFF]");
         } else if (r.laserEquipped == true && r.laserActive == false){
+            r.laserActive = true;
             System.out.println("\n[LASER ON]");
         } else {
             System.out.println("\n[ACTION FAILED]-ROOM NOT EQUIPPED WITH LASER");
         }
-    }
-
-    /**
-     * Attempt to trade bodies between two existences.
-     * Always prints restriction because no second existence present.
-     * @param e1 The first Existence
-     * @param e2 The second Existence
-     */
-    public void tradeBody(Existence e1, Existence e2) {
-        System.out.println("\nYou can only trade bodies if there is at least another existence present.");
-        // TO be contructed!!
-    }
-
-    /**
-     * Attempts self-destruction. Only a human operator may trigger it.
-     * @param operator the one pressing the button
-     * @param toBeDestroyed the robot body to destroy
-     */
-    public void selfDestruct(Human operator, Robot toBeDestroyed) {
-        System.out.println("\nSelf-destruct sequence initiated! Boom!");
-        toBeDestroyed.die();
-    }
-
-    /**
-     * (Overloaded) Robot tries to destroy another Robot
-     * @param operator the one pressing the button
-     * @param toBeDestroyed the one being destroyed
-     */
-    public void selfDestruct(Robot operator, Robot toBeDestroyed) {
-        throw new RuntimeException("\nFAILED: A robot cannot do that to another robot.");
-    }
-
-    /**
-     * (Overloaded)Robot tries to destroy a Human
-     * @param operator the one pressing the button
-     * @param toBeDestroyed the one being destroyed
-     */
-    public void selfDestruct(Robot operator, Human toBeDestroyed) {
-        throw new RuntimeException("\nFAILED: Only a human can do this to a robot.");
     }
 }
