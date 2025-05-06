ce111255f3d2de1ae8c85c0dbd32729ac1ddd3a4
Yunxian Ding <yding41@smith.edu>
2025-05-05 22:36:00 -0400

    Updated javadoc

--- Human.java
+++ Human.java
@@ -1,23 +1,25 @@
 import java.util.ArrayList;
 
 /**
- * The Human class extends Existence class and has unique features of 
- * initiating the self destruct program in a robot
+ * The Human class extends Existence class
+ * It has unique attributes of boolean isUnconscious
+ * It also has unique methods of initiating the self destruct program in a robot
  */
 public class Human extends Existence {
+    
     // Attributes
     public Boolean isUnconscious;
 
     // Constructor
 
     /**
-     * Default constructor for 'Human'
-     * @param name
-     * @param room
-     * @param isAlive
-     * @param canGoToLab
-     * @param canWalk
-     * @param canInspect
+     * constructor for Human
+     * @param name the human's name
+     * @param room the current location of the human
+     * @param isAlive the human's life status
+     * @param canGoToLab indicator for whether the human can go to lab
+     * @param canWalk indicator for whether the human can walk
+     * @param canInspect indicator for whether the human can inspect
      */
     public Human(String name, Room room, Boolean isAlive, Boolean canGoToLab, Boolean canWalk, Boolean canInspect) {
         super(name, room);
